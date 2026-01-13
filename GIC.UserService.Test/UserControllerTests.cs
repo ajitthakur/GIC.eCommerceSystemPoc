6d40e9c08e1ef58db43159e9ebf613e38809f967
THAKUR, Ajitsingh <a.thakur@unrwa.org>
2026-01-14 00:00:07 +0530

    user api test

--- GIC.UserService.Test/UserControllerTests.cs
+++ GIC.UserService.Test/UserControllerTests.cs
@@ -1,12 +1,36 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
 using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Xunit;
+
+
+public class UserDetailPartialDto 
+{
+    public UserDetailDto userDetail { get; set; }
+}
+
+
+public class UserDetailDto
+{
+    public UserDto result { get; set; }
+}
+
+
+
+public class UserDtoPartial{
+    public string id { get; set; }
+    public string name { get; set; }
+    public string email { get; set; }
+}
+
 
 public class UserDto 
 {
@@ -15,49 +39,55 @@ public class UserDto
     public string Email { get; set; }
 }
 
-public class UserControllerTests : IClassFixture<WebApplicationFactory<Program>>
+public class UserControllerTests 
 {
     private readonly HttpClient _client;
 
-    public UserControllerTests(WebApplicationFactory<Program> factory)
-    {
-        _client = factory.CreateClient(); ;
+    public UserControllerTests()
+    {        
+        _client = new HttpClient
+        {
+            BaseAddress = new System.Uri("https://localhost:5001")//User API            
+        };
     }
 
+
     [Fact]
-    public async Task CreateUser_Should_ReturnCreatedUser()
+    public async Task CreateUser_Should_Return_Created_User()
     {
         // Arrange
         var newUser = new UserDto { Name = $"ajit{Guid.NewGuid()}", Email = $"ajit{Guid.NewGuid()}@testemail.com" };
 
         // Act
-        var response = await _client.PostAsJsonAsync("http://localhost:5001/User", newUser);
+        var response = await _client.PostAsJsonAsync("User", newUser);
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.Created);
 
         var createdUser = await response.Content.ReadFromJsonAsync<UserDto>();
         createdUser.Should().NotBeNull();
-        createdUser!.Name.Should().Be("ajit");
+        createdUser!.Name.Should().Be(newUser.Name);
         createdUser.Id.Should().NotBe(Guid.Empty);
     }
 
     [Fact]
-    public async Task GetUserById_Should_ReturnUser_WhenExists()
+    public async Task GetUserById_Should_Return_User_If_Exists()
     {
         // First create a user
         var newUser = new UserDto { Name = $"ajit{Guid.NewGuid()}", Email = $"ajit{Guid.NewGuid()}@testemail.com" };
-        var createResponse = await _client.PostAsJsonAsync("http://localhost:5001/User", newUser);
+        var createResponse = await _client.PostAsJsonAsync("User", newUser);
         var createdUser = await createResponse.Content.ReadFromJsonAsync<UserDto>();
 
         // Act: fetch by ID
-        var getResponse = await _client.GetAsync($"http://localhost:5001/User/{createdUser!.Id}");
+        var getResponse = await _client.GetAsync($"User/{createdUser!.Id}");
 
         // Assert
         getResponse.StatusCode.Should().Be(HttpStatusCode.OK);
-        var fetchedUser = await getResponse.Content.ReadFromJsonAsync<UserDto>();
-        fetchedUser!.Name.Should().Be("ajit");
-        fetchedUser.Id.Should().Be(createdUser.Id);
+        var fetchedUserJson = await getResponse.Content.ReadAsStringAsync();
+        using var doc = JsonDocument.Parse(fetchedUserJson);
+        var root = doc.RootElement;        
+        var id = root.GetProperty("userDetail").GetProperty("result").GetProperty("id");                   
+        id.ToString().Should().Be(createdUser.Id.ToString());
     }
 }
 
