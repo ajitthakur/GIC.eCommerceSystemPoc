ea0ed0abf7f207b5ab0d255b14492d4b2b50f78f
THAKUR, Ajitsingh <a.thakur@unrwa.org>
2026-01-13 21:29:36 +0530

    Changes related to swagger api route in root

--- GIC.UserService/Program.cs
+++ GIC.UserService/Program.cs
@@ -38,7 +38,12 @@ var app = builder.Build();
 //if (app.Environment.IsDevelopment())
 //{
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
+
 //}
 
 app.UseHttpsRedirection();
